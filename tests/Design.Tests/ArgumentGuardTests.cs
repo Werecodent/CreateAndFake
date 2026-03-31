588f165210ce9b11bb66369ea1dff71aa0c87a5e
Werebunny <18252929+Werebunny@users.noreply.github.com>
2026-03-31 15:56:10 -0600

    Test tweaks.

--- tests/Design.Tests/ArgumentGuardTests.cs
+++ tests/Design.Tests/ArgumentGuardTests.cs
@@ -53,7 +53,7 @@ public static class ArgumentGuardTests
     [Theory, RandomData]
     public static void ThrowIfAsynchronous_TrueWithTask(string message)
     {
-        Task task = Task.Delay(100, TestContext.Current.CancellationToken);
+        Task task = Task.Delay(1000, TestContext.Current.CancellationToken);
         task.Assert(t => ArgumentGuard.ThrowIfAsynchronous(t, message))
             .Throws<AsynchronousAccessException>()
             .Message.Assert()
